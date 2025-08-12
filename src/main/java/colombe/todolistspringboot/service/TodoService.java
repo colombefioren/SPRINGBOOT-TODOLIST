e0ebb8ea26747e4fd5a5a8ddf192bea0dd25d3d5
colombefioren <colomberakotonjanahary@gmail.com>
2025-08-12 11:35:16 +0300

    refactor: correct Instant values

--- src/main/java/colombe/todolistspringboot/service/TodoService.java
+++ src/main/java/colombe/todolistspringboot/service/TodoService.java
@@ -8,9 +8,9 @@ import java.util.List;
 
 public class TodoService {
     public List<Todo> getAll(){
-        Todo todo1= new Todo(1,"Eat","Bean Soup", null,null,false);
-        Todo todo2= new Todo(2,"Dance","Hip Hop",null,null,true);
-        Todo todo3= new Todo(3,"Sleep","You need to sleep",null,null,false);
+        Todo todo1= new Todo(1,"Eat","Bean Soup", Instant.parse("2025-08-12T11:34:35Z"),Instant.parse("2025-08-12T11:34:35Z"),false);
+        Todo todo2= new Todo(2,"Dance","Hip Hop",Instant.parse("2025-08-12T11:34:35Z"),Instant.parse("2025-08-12T11:34:35Z"),true);
+        Todo todo3= new Todo(3,"Sleep","You need to sleep",Instant.parse("2025-08-12T11:34:35Z"),Instant.parse("2025-08-12T11:34:35Z"),false);
 
         return Arrays.asList(todo1,todo2,todo3);
     }
