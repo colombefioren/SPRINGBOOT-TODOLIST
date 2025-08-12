65ca6c5567439cc87079c1b0f5bde4d87f991cd9
colombefioren <colomberakotonjanahary@gmail.com>
2025-08-12 11:14:37 +0300

    feat: GET /todos

--- src/main/java/colombe/todolistspringboot/controller/TodoController.java
+++ src/main/java/colombe/todolistspringboot/controller/TodoController.java
@@ -3,10 +3,20 @@ package colombe.todolistspringboot.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class TodoController {
+    private TodoService service;
+
     @GetMapping("/ping")
     public String ping() {
         return "The sh*t is working!";
     }
+
+    @GetMapping("/todos")
+    public List<Todo> get1llTodos() {
+        return service.getAll();
+    }
+
 }
