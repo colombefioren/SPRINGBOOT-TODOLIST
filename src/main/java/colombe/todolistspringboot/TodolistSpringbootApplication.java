15403b0e790555232020400c510e087a5f09afa5
colombefioren <colomberakotonjanahary@gmail.com>
2025-08-12 10:47:51 +0300

    chore: initialize project

---
+++ src/main/java/colombe/todolistspringboot/TodolistSpringbootApplication.java
@@ -0,0 +1,12 @@
+package colombe.todolistspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodolistSpringbootApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TodolistSpringbootApplication.class, args);
+  }
+}
