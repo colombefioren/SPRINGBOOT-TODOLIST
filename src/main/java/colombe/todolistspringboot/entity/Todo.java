cc7924861e000a33964fff4404c3bd3725c8504f
colombefioren <colomberakotonjanahary@gmail.com>
2025-08-31 14:43:19 +0300

    feat: jpa annotation to Todo entity

--- src/main/java/colombe/todolistspringboot/entity/Todo.java
+++ src/main/java/colombe/todolistspringboot/entity/Todo.java
@@ -1,21 +1,36 @@
 package colombe.todolistspringboot.entity;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 import java.time.Instant;
 
+@Entity
+@Table(name = "todo")
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
 @EqualsAndHashCode
 @ToString
 public class Todo {
-    private final int id;
-    private final String title;
-    private final String description;
-    private final Instant startDatetime;
-    private final Instant endDatetime;
-    private final boolean done;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String title;
+
+    private String description;
+
+    @Column(name = "start_datetime")
+    private Instant startDatetime;
+
+    @Column(name = "end_datetime")
+    private Instant endDatetime;
+
+    private boolean done;
 }
