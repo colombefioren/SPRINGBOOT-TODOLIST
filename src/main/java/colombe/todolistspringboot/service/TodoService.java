d302739aeff6195af15025aab0c25062b903a68b
colombefioren <colomberakotonjanahary@gmail.com>
2025-08-31 14:46:32 +0300

    refactor: update todoservice to use todorepository for crud actions

--- src/main/java/colombe/todolistspringboot/service/TodoService.java
+++ src/main/java/colombe/todolistspringboot/service/TodoService.java
@@ -1,17 +1,30 @@
 package colombe.todolistspringboot.service;
 
 import colombe.todolistspringboot.entity.Todo;
+import colombe.todolistspringboot.repository.TodoRepository;
+import org.springframework.stereotype.Service;
 
-import java.time.Instant;
-import java.util.Arrays;
 import java.util.List;
 
-public class TodoService {
-    public List<Todo> getAll(){
-        Todo todo1= new Todo(1,"Eat","Bean Soup", Instant.parse("2025-08-12T11:34:35Z"),Instant.parse("2025-08-12T11:34:35Z"),false);
-        Todo todo2= new Todo(2,"Dance","Hip Hop",Instant.parse("2025-08-12T11:34:35Z"),Instant.parse("2025-08-12T11:34:35Z"),true);
-        Todo todo3= new Todo(3,"Sleep","You need to sleep",Instant.parse("2025-08-12T11:34:35Z"),Instant.parse("2025-08-12T11:34:35Z"),false);
 
-        return Arrays.asList(todo1,todo2,todo3);
+@Service
+public class TodoService {
+
+    private final TodoRepository repository;
+
+    public TodoService(TodoRepository repository) {
+        this.repository = repository;
     }
+    public List<Todo> getAllTodos() {
+        return repository.findAll();
+    }
+
+    public Todo saveTodo(Todo todo) {
+        return repository.save(todo);
+    }
+
+    public void deleteTodo(Integer id) {
+        repository.deleteById(id);
+    }
+
 }
